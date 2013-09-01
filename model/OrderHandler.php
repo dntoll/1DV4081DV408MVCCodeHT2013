08b1ea3c01c84beb48b896887c84ff142ef7ac72
Daniel Toll <daniel@familjentoll.se>
2013-09-01 17:38:32 +0200

    Pretty Print sortof

--- model/OrderHandler.php
+++ model/OrderHandler.php
@@ -3,7 +3,34 @@
 namespace model;
 
 class OrderHandler {
+
 	public function placeOrder(ShoppingCart $cart, Adress $adress) {
-		
+		$cartString = $this->getCartString($cart);
+		$adressString = $adress->getAdress();
+		file_put_contents("orders.txt", $cartString . $adressString, FILE_APPEND | LOCK_EX);
+	}
+
+
+	private function getCartString(ShoppingCart $cart) {
+		$productLines = $cart->getProductLines();
+
+		$ret = "
+		***************************
+		Order \n";
+		foreach ($productLines  as $productLine) {
+			$product = $productLine->getProduct();
+			$count = $productLine->getCount();
+			$productName = $product->getName();
+			$productCode = $product->getCode();
+			$priceSEK = $product->getPriceSEK();
+			$lineTotal = $productLine->getLineTotal();
+			$ret .= "$productName  $count * $priceSEK = $lineTotal :-\n";
+			
+		}
+		$orderTotal = $cart->getTotalPrice();
+
+		$ret .= "Total: $orderTotal :-
+		***************************\n";
+		return $ret;
 	}
 }
